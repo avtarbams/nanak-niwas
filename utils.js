c33e551681091ebb4bdc82876bdfd86c0913da42
Avtar Singh Bamra <31774649+avtarbams@users.noreply.github.com>
2018-12-31 12:25:10 -0500

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,28 @@
+import { withInfo } from '@storybook/addon-info';
+const wInfoStyle = {
+  header: {
+    body: {
+      paddingBottom: 0,
+      paddingTop: 0
+      
+    },
+    h1: {
+      display: 'inline',
+      fontSize: '25px',
+      marginRight: '20px'
+    },
+    
+    h2: {
+      color: '#999',
+      display: 'inline'
+      
+    }
+  },
+  infoBody: {
+    backgroundColor: '#eee',
+    lineHeight: '2',
+    padding: '0px 5px'
+  }
+};
+export const wInfo = text =>
+  withInfo({ inline: true, source: false, styles: wInfoStyle, text: text });
